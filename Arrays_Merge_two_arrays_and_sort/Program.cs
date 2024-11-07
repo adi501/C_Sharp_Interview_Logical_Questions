8985330ce10fac83f4be3b9e9dd09c71ef68b672
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-11-07 18:55:25 +0530

    Merge two arrays and sort  in c#

---
+++ Arrays_Merge_two_arrays_and_sort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Merge_two_arrays_and_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 3, 1, 2 };
+            int[] B = { 4, 6, 5 };
+            int[] c = a.Concat(B).ToArray();
+            Array.Sort(c);
+            foreach (int i in c)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
