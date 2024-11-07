65ec61cf1bfbdf2b320fd8eb1f80e75b59c234d7
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-11-07 18:36:19 +0530

    Palindrome_String

---
+++ Palindrome_String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "abcba";
+            string reverse = "";
+            for (int i = givenString.Length; i >0; i--)
+            {
+                reverse += givenString[i-1];
+            }
+            if (givenString == reverse)
+            {
+                Console.WriteLine("String is a palindrome \n");
+            }
+            else
+            {
+                Console.WriteLine("String not is a palindrome \n");
+            }
+        }
+    }
+}
