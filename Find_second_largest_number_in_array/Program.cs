80ac164273b385602d9824a2c16fd5918a50df90
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-11-20 18:49:07 +0530

    find second largest number in array in c#

---
+++ Find_second_largest_number_in_array/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int[] arr = {1, 2, 3,9,10, 4, 5};
+Array.Sort(arr);
+int a = 0, b = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (a < arr[i])
+        a = arr[i];
+}
+for (int i = 0; i < arr.Length; i++)
+{
+    if (b<arr[i] && a!=arr[i])
+        b=arr[i];
+}
+Console.WriteLine("Second Largest Number: "+b);
+Console.ReadLine();
---
+++ Keywords_Virtual_Override_New_example/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keywords_Virtual_Override_New_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            parent objp = new parent();
+            objp.Method1();
+            objp.Method2();
+            objp.Method3();
+
+            Console.WriteLine("==================");
+
+            child objc = new child();
+            objc.Method1();
+            objc.Method2();
+            objc.Method3();
+
+            Console.WriteLine("==================");
+            parent objpc = new child();
+            objpc.Method1();
+            objpc.Method2();
+            objpc.Method3();
+
+           
+
+            Console.ReadLine();
+
+        }
+
+       
+    }
+    public class parent
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Parent Method1");
+        }
+        public virtual void Method2()
+        {
+            Console.WriteLine("Parent Method2");
+        }
+        public virtual void Method3()
+        {
+            Console.WriteLine("Parent Method3");
+        }
+    }
+    public class child:parent
+    {
+        public void Method1()
+        {
+            Console.WriteLine("child Method1");
+        }
+        public override void Method2()
+        {
+            Console.WriteLine("child Method2");
+        }
+        public new void Method3()
+        {
+            Console.WriteLine("child Method3");
+        }
+    }
+}
