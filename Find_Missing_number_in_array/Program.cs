c0dd73f0e573ed83665ccd16cef890cfe82eb185
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-11-20 19:13:20 +0530

    find missing number in array using c#

---
+++ Find_Missing_number_in_array/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] arr = { 1, 2, 9, 10, 4, 5 };
+Array.Sort(arr);
+int a = arr[0], b = arr[arr.Length - 1];
+bool status=false;
+for(int i = a; i <=b; i++)
+{
+    for(int j = 0; j < arr.Length; j++)
+    {
+        if (i == arr[j])
+        {
+            status = true;
+            break;
+        }
+        else
+        {
+            status = false;
+        }
+    }
+    if (status==false)
+    {
+        Console.WriteLine(i);
+    }
+}
+Console.ReadLine();
