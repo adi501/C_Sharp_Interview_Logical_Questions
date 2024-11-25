b6f27c023c4d2ef69246b15c76a52452874ec70e
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-11-25 19:14:58 +0530

    Fibonacci Series Example

---
+++ Fibonacci_Series_Example/Program.cs
@@ -0,0 +1,15 @@
+﻿//Fibonacci_Series_Example
+//0,1,1,2,3,5,8,13,21,34 (upto 50)
+
+int firstNum = 0, secondNumber = 1;
+Console.WriteLine(firstNum);
+Console.WriteLine(secondNumber);
+int result=firstNum+secondNumber;
+while(result<50)
+{
+    Console.WriteLine(result);
+    firstNum = secondNumber;
+    secondNumber = result;
+    result = firstNum + secondNumber;
+}
+Console.ReadLine();
