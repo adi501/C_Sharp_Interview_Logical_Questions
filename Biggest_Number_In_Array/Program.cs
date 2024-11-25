1d83cc1391f836120d22b9dd5048643aafe58c5d
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-11-25 19:45:39 +0530

    biggest number in array

---
+++ Biggest_Number_In_Array/Program.cs
@@ -0,0 +1,13 @@
+﻿//biggest number in array
+
+int[] arr= {1,2,3,9,5,6,7};
+int bigNmuber = arr[0];
+for (int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] > bigNmuber)
+    {
+        bigNmuber = arr[i];
+    }
+}
+Console.WriteLine("Biggest Number in given Array: " + bigNmuber);
+Console.ReadLine();
