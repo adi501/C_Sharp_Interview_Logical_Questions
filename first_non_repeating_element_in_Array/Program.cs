f4060f3f034e8db708e79b69c1f6c5a3363ae6c9
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-12 15:38:23 +0530

    first non-repeating element in a given Array

---
+++ first_non_repeating_element_in_Array/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine(findNonRepeating([1, 2, 1, 3, 4, 3, 4]));
+Console.ReadLine();
+ int findNonRepeating(int[] input)
+{
+    for(int i = 0;i<input.Length;i++)
+    {
+        int j;
+        for ( j= 0; j < input.Length; j++)
+        {
+            if (i!=j && input[i] == input[j])
+            {
+                break;
+            }
+        }
+        if (j == input.Length)
+            return input[i];
+    }
+    return -1;
+}
