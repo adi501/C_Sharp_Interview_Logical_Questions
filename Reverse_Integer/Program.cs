30fa8b329cb6ed14a1b7777b70d3822f2183fac1
Adinarayana Reddy JC <jc.adi101@gmail.com>
2025-01-01 21:14:34 +0530

    Reverse Integer in c#

---
+++ Reverse_Integer/Program.cs
@@ -0,0 +1,11 @@
+﻿// input: 12345 // output: 54321
+int input = 12345;
+int output = 0;
+while (input != 0)
+{
+    int remainder = input % 10;
+    output = output * 10 + remainder;
+    input = input / 10;
+}
+Console.WriteLine(output);  //54321
+Console.ReadLine();
