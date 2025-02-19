421b35d564a95ed5472289b6f212cb77ffa634c0
Adinarayana Reddy JC <jc.adi101@gmail.com>
2025-02-19 17:56:41 +0530

    find the longest substring without repeating characters

---
+++ Longest_Substring_without_repeating_characters/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Input: "abcabcbb" find the longest substring without repeating characters.
+//Output: The longest substring is "abc".
+
+string input = "abcabcbb";
+List<string> substrings = new List<string>();
+for (int i = 0; i < input.Length; i++)
+{
+    string substring = "";
+    for (int j = i; j < input.Length; j++)
+    {
+        if (substring.Contains(input[j]))
+        {
+            break;
+        }
+        substring += input[j];
+    }
+    substrings.Add(substring);
+
+}
+var a = substrings.OrderByDescending(x => x.Length).FirstOrDefault();
+Console.WriteLine($"The longest substring is \"{a}\".");
+Console.ReadLine();
