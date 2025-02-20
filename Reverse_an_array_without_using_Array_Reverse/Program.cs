148a5b367f2bbd749879dfe4b6749afb1ceff28b
Adinarayana Reddy JC <jc.adi101@gmail.com>
2025-02-20 12:05:55 +0530

    Reverse_an_array_without_using_Array_Reverse

---
+++ Reverse_an_array_without_using_Array_Reverse/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+for (int i = 0; i < arr.Length / 2; i++)
+{
+    //Methods 1
+    //int tmp = arr[i];
+    //arr[i] = arr[arr.Length - i - 1];
+    //arr[arr.Length - i - 1] = tmp;
+
+    //Methods 2
+    //Note : assume a=arr[i] and b=arr[arr.Length - i - 1] , then a=a+b, b=a-b, a=a-b
+    arr[i] = arr[i] + arr[arr.Length - i - 1]; //a=a+b
+    arr[arr.Length - i - 1] = arr[i] - arr[arr.Length - i - 1]; //b=a-b
+    arr[i] = arr[i] - arr[arr.Length - i - 1]; //a=a-b
+}
+Console.WriteLine(string.Join(",", arr));
